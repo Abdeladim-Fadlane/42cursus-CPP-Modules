b54f4caca99f7e9d13f48fec6c7f811f7ce00f71
Abdeladim Fadlane <afadlane@c3r6p6.1337.ma>
2023-08-31 18:53:28 +0100

    update

--- module02/ex00/main.cpp
+++ module02/ex00/main.cpp
@@ -1,100 +1,139 @@
-// #include<iostream>
+// // #include<iostream>
 
-// class A
+// // class A
+// // {
+// //     private:
+// //         int *x;
+// //         int *y;
+// //     public:
+// //         A();
+// //         void __printf()
+// //         {
+// //             std::cout<<*(this->x )<<" , ";
+// //             std::cout<<*(this->y )<<"\n";
+// //         }
+// //         A&operator=(const A& abdo)//copy assignment operator
+// //         {
+// //             this->x = abdo.x;
+// //             //this->y = abdo.y;
+// //             return *this;
+// //         }
+// //         void    setPoint(int i , int j)
+// //         {
+// //             *(this->x ) = i;
+// //             *(this->y ) = j;
+// //         }
+// // };
+// // A::A()
+// // {
+// //     this->x = new int;
+// //     *(this->x )= 0;
+// //     this->y = new int;
+// //     *(this->y )= 0;  
+// // }
+
+// // int main()
+// // {
+// //     A a,s;
+// //     s = a;
+// //     a.setPoint(12,23);
+// //     a.__printf();
+// //     s.__printf();
+// // }
+// #include <cassert>
+// #include <iostream>
+
+// class Fraction
 // {
-//     private:
-//         int *x;
-//         int *y;
-//     public:
-//         A();
-//         void __printf()
-//         {
-//             std::cout<<*(this->x )<<" , ";
-//             std::cout<<*(this->y )<<"\n";
-//         }
-//         A&operator=(const A& abdo)//copy assignment operator
-//         {
-//             this->x = abdo.x;
-//             //this->y = abdo.y;
-//             return *this;
-//         }
-//         void    setPoint(int i , int j)
-//         {
-//             *(this->x ) = i;
-//             *(this->y ) = j;
-//         }
+// private:
+// 	int m_numerator { 0 };
+// 	int m_denominator { 1 };
+
+// public:
+// 	// Default constructor
+// 	Fraction(int numerator = 0, int denominator = 1 )
+// 		: m_numerator { numerator }, m_denominator { denominator }
+// 	{
+// 		assert(denominator != 0);
+// 	}
+
+// 	// Copy constructor
+// 	Fraction(const Fraction& copy)
+// 		: m_numerator { copy.m_numerator }, m_denominator { copy.m_denominator }
+// 	{
+// 		// no need to check for a denominator of 0 here since copy must already be a valid Fraction
+// 		std::cout << "Copy constructor called\n"; // just to prove it works
+// 	}
+
+// 	// Overloaded assignment
+// 	Fraction& operator= (const Fraction& fraction);
+
+// 	//friend std::ostream& operator<<(std::ostream& out, const Fraction& f1);
+
 // };
-// A::A()
+
+// // std::ostream& operator<<(std::ostream& out, const Fraction& f1)
+// // {
+// // 	out << f1.m_numerator << '/' << f1.m_denominator;
+// // 	return out;
+// // }
+
+// // A simplistic implementation of operator= (see better implementation below)
+// Fraction& Fraction::operator= (const Fraction& fraction)
 // {
-//     this->x = new int;
-//     *(this->x )= 0;
-//     this->y = new int;
-//     *(this->y )= 0;  
+//     // do the copy
+//     m_numerator = fraction.m_numerator;
+//     m_denominator = fraction.m_denominator;
+
+//     // return the existing object so we can chain this operator
+//     return *this;
 // }
 
 // int main()
 // {
-//     A a,s;
-//     s = a;
-//     a.setPoint(12,23);
-//     a.__printf();
-//     s.__printf();
+//     Fraction fiveThirds { 5, 3 };
+//     Fraction f;
+//     f = fiveThirds; // calls overloaded assignment
+//     std::cout << f;
+
+//     return 0;
 // }
-#include <cassert>
+
+
 #include <iostream>
 
-class Fraction
-{
+class MyClass {
 private:
-	int m_numerator { 0 };
-	int m_denominator { 1 };
+    int value;
 
 public:
-	// Default constructor
-	Fraction(int numerator = 0, int denominator = 1 )
-		: m_numerator { numerator }, m_denominator { denominator }
-	{
-		assert(denominator != 0);
-	}
+    MyClass(int val) : value(val) {}
 
-	// Copy constructor
-	Fraction(const Fraction& copy)
-		: m_numerator { copy.m_numerator }, m_denominator { copy.m_denominator }
-	{
-		// no need to check for a denominator of 0 here since copy must already be a valid Fraction
-		std::cout << "Copy constructor called\n"; // just to prove it works
-	}
+    // Copy assignment operator overload
+    MyClass& operator=(const MyClass& other) {
+        if (this != &other) {
+            value = other.value;
+        }
+        return *this;
+    }
 
-	// Overloaded assignment
-	Fraction& operator= (const Fraction& fraction);
-
-	//friend std::ostream& operator<<(std::ostream& out, const Fraction& f1);
-
+    int getValue() const {
+        return value;
+    }
 };
 
-// std::ostream& operator<<(std::ostream& out, const Fraction& f1)
-// {
-// 	out << f1.m_numerator << '/' << f1.m_denominator;
-// 	return out;
-// }
+int main() {
+    MyClass obj1(10);
+    MyClass obj2(20);
 
-// A simplistic implementation of operator= (see better implementation below)
-Fraction& Fraction::operator= (const Fraction& fraction)
-{
-    // do the copy
-    m_numerator = fraction.m_numerator;
-    m_denominator = fraction.m_denominator;
+    std::cout << "obj1 value: " << obj1.getValue() << std::endl;
+    std::cout << "obj2 value: " << obj2.getValue() << std::endl;
 
-    // return the existing object so we can chain this operator
-    return *this;
-}
+    obj2 = obj1; // Copy assignment
 
-int main()
-{
-    Fraction fiveThirds { 5, 3 };
-    Fraction f;
-    f = fiveThirds; // calls overloaded assignment
-    std::cout << f;
+    std::cout << "After copy assignment:" << std::endl;
+    std::cout << "obj1 value: " << obj1.getValue() << std::endl;
+    std::cout << "obj2 value: " << obj2.getValue() << std::endl;
 
     return 0;
 }
